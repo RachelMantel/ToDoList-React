d2ddf71cd8e8570d453a79cee970c973b370cd3d
RachelMantel <rrm85040@gmail.com>
2025-02-06 15:15:44 +0200

    fix

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:5065"; 
 
+axios.defaults.baseURL =process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(
   response => {
